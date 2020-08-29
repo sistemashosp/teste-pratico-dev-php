27751e7db97fcbee10c8f541e8bdf2692a0e3738
andrebaldissara <69902998+andrebaldissara@users.noreply.github.com>
2020-08-29 10:57:25 -0300

    Add files via upload

--- lista.php
+++ lista.php
@@ -1,1 +1,18 @@
-<?php echo "Seu código aqui"; ?>
+<?php echo  require_once 'cadastra.php';
+
+$dados = new Dados();
+
+$select = new Select();
+
+echo 'Nome ($select->$nome)';
+echo 'sobrenome ($select->$sobrenome)';
+echo 'cpf ($select->$cpf)';
+echo 'email ($select->$email)';
+echo 'nascimento ($select->$nascimento)';
+echo 'genero ($select->$genero)';
+echo 'endereco ($select->$endereco)';
+echo 'cidade ($select->$cidade)';
+echo 'estado ($select->$estado)';
+echo 'cep ($select->$cep)';
+echo 'descricao ($select->$descricao)';
+}; ?>
